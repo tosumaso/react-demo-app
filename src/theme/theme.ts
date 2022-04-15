2e7dcfb621d77984abb46e3b60e1361c19b1f841
tosumaso <wakuwaku5712@gmail.com>
2022-04-15 17:19:20 +0900

    first commit

---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "grey.100",
+        color: "gray.800"
+      }
+    }
+  }
+})
+
